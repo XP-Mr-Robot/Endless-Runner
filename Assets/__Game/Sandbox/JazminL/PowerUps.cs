8bbea2f31ebdd49cdc84e526aff9c94cbed64e17
Jazmin <ijazmin98@hotmail.com>
2020-10-22 22:20:53 -0500

    PowerUp vida (testing)

---
+++ Assets/__Game/Sandbox/JazminL/PowerUps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public GameObject vida;
+    public float MasUno = 1f;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PickUp(other);
+        }
+    }
+
+    public void PickUp(Collider player)
+    {
+        Instantiate(vida, transform.position, transform.rotation);
+
+        PlayerStats stats = player.GetComponent<PlayerStats>();
+        stats.health += MasUno;
+
+
+            
+
+    }
+
+
+}
