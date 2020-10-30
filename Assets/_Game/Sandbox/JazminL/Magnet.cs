58925f4eba8188e3c2b909fe839a2411af79409e
Leo <leonsanchez09@gmail.com>
2020-10-30 03:14:33 -0600

    Se prepara proyecto para merge con design

---
+++ Assets/_Game/Sandbox/JazminL/Magnet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+
+    public GameObject coinMagnet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinMagnet = GameObject.FindGameObjectWithTag("Coin Magnet");
+        coinMagnet.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(ActivateCoins());
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+
+    IEnumerator ActivateCoins()
+    {
+        coinMagnet.SetActive(true);
+        yield return new WaitForSeconds(7);
+        coinMagnet.SetActive(false);
+    }
+}
