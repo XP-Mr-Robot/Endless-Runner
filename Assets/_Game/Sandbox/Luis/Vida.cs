32adec47754105c88ecd3389a606d457909e6cc2
Leo <leonsanchez09@gmail.com>
2020-11-18 03:31:26 -0600

    Script de Vida funcionando correctamente
    Se soluciona el bug de la vida haciendo que se compare el ID de cada gameObject

--- Assets/_Game/Sandbox/Luis/Vida.cs
+++ Assets/_Game/Sandbox/Luis/Vida.cs
@@ -5,49 +5,45 @@ using UnityEngine;
 public class Vida : MonoBehaviour
 {
     public static int vidas = 3;
-
-    // Start is called before the first frame update
+    int lastCollision;
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
-    /* public void OnTriggerEnter(Collider col)
-     {
-         switch (col.gameObject.tag)
-         {
-             case "obstaculo":
-                 vidas -= 1;
-                 Debug.Log("tienes " + vidas + " vidas");
-                 if (vidas <= 0)
-                 {
-                     Debug.Log("Game over");
-                     Destroy(gameObject);
-                 }
-                 break;
-         }
-
-     }
-    */
+   
 
     public void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        switch (hit.gameObject.tag)
-        {
-            case "obstaculo":
-                vidas -= 1;
-                Debug.Log("tienes " + vidas + " vidas");
-                if (vidas <= 0)
-                {
-                    Debug.Log("Game over");
-                    Destroy(gameObject);
-                }
+          switch (hit.gameObject.tag)
+           {
+               case "obstaculo":
+                if (lastCollision != hit.gameObject.GetInstanceID())
+                    TakeDamage();
+                lastCollision = hit.gameObject.GetInstanceID();
                 break;
+
+
+           }
+        
+
+    }
+
+    private void TakeDamage()
+    {
+        vidas -= 1;
+        Debug.Log("tienes " + vidas + " vidas");
+        if (vidas <= 0)
+        {
+            Debug.Log("Game over");
+            Destroy(gameObject);
         }
     }
+
+
+
+   
 }
