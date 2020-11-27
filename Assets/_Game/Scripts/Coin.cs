f8f42c4f99f8c1ac0f10eb20f939162c0e5026b9
Michelle Otea <michelleotea@gmail.com>
2020-11-27 01:53:07 -0600

    PowerUp x2

--- Assets/_Game/Scripts/Coin.cs
+++ Assets/_Game/Scripts/Coin.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    public bool DoublePickUp;
+    public int points;
 
     private void Start()
     {
+        points = 1;
+    }
+
+    private void Update()
+    {
+        if (DoublePickUp = true)
+        {
+            points = 2;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
+
+
         if(other.tag == "Player")
         {
-            GameObject.Find("Clock").GetComponent<CoinCounter>().numberOfCoins +=1;
+          
+
+            GameObject.Find("Clock").GetComponent<CoinCounter>().numberOfCoins += points;
             Debug.Log("Coins:" + GameObject.Find("Clock").GetComponent<CoinCounter>().numberOfCoins);
             GameObject.Find("GameManager").GetComponent<PlayerManager>().coinSound.Play();
             Destroy(gameObject);
+
+
         }
     }
     
--- Assets/_Game/Scripts/CoinCounter.cs
+++ Assets/_Game/Scripts/CoinCounter.cs
@@ -7,14 +7,37 @@ public class CoinCounter : MonoBehaviour
 {
     public int numberOfCoins = 0;
     [SerializeField] private Text m_Text = null;
+
+    
+
     void Start()
     {
-        
+        DoublePickUp = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.m_Text.text = this.numberOfCoins.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Double")
+        {
+            DoublePickUp = true;
+            Destroy(gameObject);
+           
+        }
+
+        if (other.tag == "Coin" && !DoublePickUp)
+        numberOfCoins += 1;
+
+        if (other.tag == "Coin" && DoublePickUp)
+        numberOfCoins += 2;
+        
+
+
+        
     }
 }
