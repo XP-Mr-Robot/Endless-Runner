fd2f49285c555d87adc3d26923051ceca85bbf76
Leo <leonsanchez09@gmail.com>
2020-12-09 09:59:41 -0600

    Se agregan PowerUps y Particulas

---
+++ Assets/_Game/Scripts/PowerUP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP : MonoBehaviour
+{
+
+    private void Start()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            GameObject.Find("Clock").GetComponent<CoinCounter>().numberOfCoins +=10;
+            GameObject.Find("TotalScore").GetComponent<TotalScore>().numberOfCoins += 10;
+            Debug.Log("Coins:" + GameObject.Find("Clock").GetComponent<CoinCounter>().numberOfCoins);
+            GameObject.Find("GameManager").GetComponent<PlayerManager>().coinSound.Play();
+            Destroy(gameObject);
+        }
+        
+           
+    }
+    
+
+
+
+}
